1b9ca606bb3fb7ed2963e5002584529de0490fab
Muhammet Çeneli <muhammetceneli@gmail.com>
2019-04-20 17:12:47 +0300

    düşman değerek öldürülüyor

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    GameObject enemy;
+
+    void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetType() == typeof(CapsuleCollider))
+        {
+            Debug.Log("degdi");
+            Destroy(enemy);
+        }
+    }
+
+}
